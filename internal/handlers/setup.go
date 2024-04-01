5df21c059c5746c13561b61db1c845fd0a9ccdfa
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-04-01 19:50:01 +0200

    refactor: add pagination

--- internal/handlers/setup.go
+++ internal/handlers/setup.go
@@ -31,15 +31,17 @@ func (setupHander *SetupHandler) GetSetupPage(c echo.Context) error {
 	username := c.Get(auth.Username_Key).(string)
 	token := c.Get(auth.GithubToken).(string)
 
+	afterCursor := c.QueryParam("after")
+
 	client := setupHander.githubService.CreateClient(context.Background(), token)
 
-	query, err := setupHander.githubService.GetUserRepositories(client, username, services.GithubPaginationParams{PageSize: 100, After: ""})
+	query, err := setupHander.githubService.GetUserRepositories(client, username, services.GithubPaginationParams{PageSize: 10, After: afterCursor})
 
 	if err != nil {
 		return err
 	}
 
-	component := pages.SetupPage(*query)
+	component := pages.SetupPage(query.User.Repositories.Nodes, query.User.Repositories.TotalCount, query.User.Repositories.PageInfo.EndCursor, query.User.Repositories.PageInfo.HasNextPage)
 	return component.Render(context.Background(), c.Response().Writer)
 
 }
--- view/pages/setup_templ.go
+++ view/pages/setup_templ.go
@@ -14,8 +14,9 @@ import "github.com/Cahllagerfeld/go-htmx-first-steps/view/layout"
 
 import "github.com/Cahllagerfeld/go-htmx-first-steps/internal/graphqlquery"
 import "strconv"
+import "fmt"
 
-func SetupPage(repos graphqlquery.RepositoryQuery) templ.Component {
+func SetupPage(repos []graphqlquery.Repository, totalCount int32, afterCursor string, hasNextPage bool) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
@@ -34,24 +35,24 @@ func SetupPage(repos graphqlquery.RepositoryQuery) templ.Component {
 				templ_7745c5c3_Buffer = templ.GetBuffer()
 				defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"flex flex-col min-h-screen w-full items-center justify-center\"><h1 class=\"text-lg font-semibold\">Please select your repos (")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"flex flex-col gap-8 min-h-screen w-full items-center justify-center\"><h1 class=\"text-lg font-semibold\">Please select your repos (")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var3 string
-			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(int(repos.User.Repositories.TotalCount)))
+			templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(strconv.Itoa(int(totalCount)))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/pages/setup.templ`, Line: 10, Col: 118}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/pages/setup.templ`, Line: 11, Col: 94}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(")</h1><ul>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(")</h1><input placeholder=\"Search\" class=\"text-zinc-950 px-2 rounded-md py-2\"><ul class=\"space-y-4\" id=\"repo-list\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			for _, item := range repos.User.Repositories.Nodes {
+			for _, item := range repos {
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<li>")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
@@ -59,7 +60,7 @@ func SetupPage(repos graphqlquery.RepositoryQuery) templ.Component {
 				var templ_7745c5c3_Var4 string
 				templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(item.Name)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/pages/setup.templ`, Line: 14, Col: 17}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `view/pages/setup.templ`, Line: 16, Col: 17}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 				if templ_7745c5c3_Err != nil {
@@ -70,7 +71,25 @@ func SetupPage(repos graphqlquery.RepositoryQuery) templ.Component {
 					return templ_7745c5c3_Err
 				}
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul></div>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			if hasNextPage {
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<button hx-get=\"")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(fmt.Sprintf("/setup?after=%s", afterCursor)))
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" hx-trigger=\"click\" hx-target=\"#repo-list\" hx-swap=\"beforeend\">Load more</button>")
+				if templ_7745c5c3_Err != nil {
+					return templ_7745c5c3_Err
+				}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
